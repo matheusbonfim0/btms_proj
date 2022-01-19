80fe9fae86e60982a33117c8ed054fb73d0083d0
Igor Pires <igor.pires@binamik.com.br>
2022-01-19 11:50:32 -0400

    atualizando arquivos

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/matheus/Documentos/python/btms_proj/.venv"
+set -gx VIRTUAL_ENV "/home/igor/Documentos/python/btms_proj/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
